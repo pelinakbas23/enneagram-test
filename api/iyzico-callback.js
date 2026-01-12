5dff8134d263c1dcf67ded25e2bc798ddd3cc475
Pelin Akbas <akbaspelin34@gmail.com>
2026-01-12 20:18:24 +0300

    Redirect after iyzico callback

--- api/iyzico-callback.js
+++ api/iyzico-callback.js
@@ -1,6 +1,20 @@
 module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.end("OK");
+  // Token bazen query’de bazen body’de gelir
+  var token = null;
+
+  if (req.query && req.query.token) token = req.query.token;
+  else if (req.body && req.body.token) token = req.body.token;
+  else if (req.body && req.body.checkoutFormToken) token = req.body.checkoutFormToken;
+
+  // Burayı istediğin sayfaya çevir:
+  // - payment.html: "ödeme alındı" mesajı göster
+  // - test_v3.html: direkt teste geçiş
+  // - welcome.html: "OANDA Enneagram Kişilik Testi’ne hoş geldiniz." sayfası
+  var target = "/payment.html?success=1";
+  if (token) target += "&token=" + encodeURIComponent(token);
+
+  res.statusCode = 302;
+  res.setHeader("Location", target);
+  res.end();
 };
 
